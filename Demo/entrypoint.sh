5c9f5ca16dd72707488bbc949cbd8bd0ce927ae9
Aureliedjied <aurelie.Djied@gmail.com>
2024-11-25 17:26:38 +0100

    entrypoint.sh

---
+++ Demo/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Utiliser envsubst pour remplacer les valeurs par des variables d'environnement dans index.html
+envsubst < /usr/local/apache2/htdocs/index.html > /usr/local/apache2/htdocs/index_temp.html
+mv /usr/local/apache2/htdocs/index_temp.html /usr/local/apache2/htdocs/index.html
+
+# Démarrer Apache en arriere plan
+exec httpd-foreground
